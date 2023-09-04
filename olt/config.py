6d7590e4edc5ed7d0bf75be856787f0ac0a52d08
MedericFourmy <mederic.fourmy@gmail.com>
2023-09-04 19:59:40 +0200

    More customization of modalities, update pyicg first

--- olt/config.py
+++ olt/config.py
@@ -1,6 +1,6 @@
 import os
 from pathlib import Path
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 import logging
 
 DATASET_NAMES = ['ycbv', 'rotd']
@@ -12,19 +12,34 @@ MEGAPOSE_DATA_DIR = Path(MEGAPOSE_DATA_DIR)
 BOP_DS_DIRS = {ds_name: MEGAPOSE_DATA_DIR / Path('bop_datasets') / ds_name for ds_name in DATASET_NAMES}
 OBJ_MODEL_DIRS = {ds_name: MEGAPOSE_DATA_DIR / Path('urdfs') / ds_name for ds_name in DATASET_NAMES}
 
+# Adapted from https://stackoverflow.com/questions/53632152/why-cant-dataclasses-have-mutable-defaults-in-their-class-attributes-declaratio
+default_list = lambda l: field(default_factory=lambda: l)
 
+"""
+Default value taken from ICG YCBV evaluation
+"""
 @dataclass
 class TrackerConfig:
     tmp_dir_name: str = 'tmp'
     viewer_name: str = 'normal_viewer'
-    n_corr_iterations: int = 3
-    n_update_iterations: int = 5
     viewer_display: bool = False
     viewer_save: bool = False
-    tikhonov_parameter_rotation: float = 1000.0
-    tikhonov_parameter_translation: float = 30000.0
     use_depth: bool = False
     measure_occlusions: bool = False
+    
+    # Optimization params
+    n_corr_iterations: int = 4
+    n_update_iterations: int = 2
+    tikhonov_parameter_rotation: float = 1000.0
+    tikhonov_parameter_translation: float = 30000.0
+
+    # RegionModality params (partial)
+    region_scales: list[int] = default_list([7, 4, 2])
+    region_standard_deviations: list[float] = default_list([25.0, 15.0, 10.0])
+
+    # DepthModality params (partial)
+    depth_considered_distances: list[float] = default_list([0.07, 0.05, 0.04])
+    depth_standard_deviations: list[float] = default_list([0.05, 0.03, 0.02])
 
 
 @dataclass
